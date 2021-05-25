5d29f34f85c6e8570145652e2093eb0ec0398c12
Finn Perry <finnperry3@gmail.com>
2021-05-25 18:53:08 +1000

    Add GLFW.

--- fae/src/foo.cpp
+++ fae/src/foo.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <GLFW/glfw3.h>
+
+#include "foo.hpp"
+
+using namespace std::chrono_literals;
 
 namespace fae
 {
 void foo()
 {
-    std::cout << "hi\n";
+    glfwInit();
+    auto window = glfwCreateWindow(1280, 720, "test window", nullptr, nullptr);
+    std::this_thread::sleep_for(2s);
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 }
