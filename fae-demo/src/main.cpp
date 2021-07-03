4d8d75e378f59c9e9be50351dc6278d942d81fcf
Finn Perry <finnperry3@gmail.com>
2021-07-03 13:27:19 +1000

    Add glad lib and window functions.

--- fae-demo/src/main.cpp
+++ fae-demo/src/main.cpp
@@ -3,8 +3,11 @@
 int main()
 {
     fae::window win{"Test Window", 1280, 720};
+    win.bind();
     while (!win.should_close())
     {
         fae::window::poll_events();
+        win.clear();
+        win.update();
     }
 }
