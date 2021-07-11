c18c0ec48fe76307953dd47f3882d387eb70a1a0
Finn Perry <finnperry3@gmail.com>
2021-07-11 15:11:13 +1000

    Move old demo code into the root_entity class.

--- fae-demo/src/main.cpp
+++ fae-demo/src/main.cpp
@@ -1,114 +1,51 @@
 #include <memory>
 
-#include <fae/application.hpp>
-#include <fae/entity.hpp>
-#include <fae/logger.hpp>
-#include <fae/window.hpp>
+#include <fae/fae.hpp>
 
 namespace
 {
-
-#if false
-
-class test_app : public fae::application
-{
-public:
-    test_app():
-        fae::application()
-    {
-    }
-
-    virtual ~test_app() = default;
-
-    virtual void load() override
-    {
-        shader = std::make_unique<fae::shader>();
-
-        mesh = std::make_unique<fae::mesh>();
-        fae::vertex vert_data[3]
-        {
-            {0.0f, 0.5f},
-            {0.5f, -0.5f},
-            {-0.5f, -0.5f}
-        };
-        mesh->set_verts(vert_data, 3);
-        fae::triangle tri_data[1]
-        {
-            {0, 1, 2}
-        };
-        mesh->set_tris(tri_data, 1);
-
-        fae::log("test", "log", "message");
-        fae::log(fae::log_type::warning, "test", "log", "warning");
-        fae::log(fae::log_type::error, "test", "log", "error");
-
-        get_window()->on_resize.add_callback([](int width, int height)->void
-        {
-            fae::log("window resize", width, height);
-        });
-    }
-
-    virtual void unload() override
-    {
-        mesh.reset();
-        shader.reset();
-    }
-
-    virtual void update() override
-    {
-        shader->bind();
-        get_renderer()->draw_mesh(*mesh.get());
-    }
-
-private:
-    std::unique_ptr<fae::shader> shader;
-    std::unique_ptr<fae::mesh> mesh;
-};
-
-
-#endif
 
 class root_entity : public fae::entity
 {
 public:
     virtual void load(update_args & args) override
     {
-        fae::log("load");
-        count = 0;
-        func = [](int w, int h)->void
+        shader_ = std::make_unique<fae::shader>();
+
+        mesh_ = std::make_unique<fae::mesh>();
+        fae::vertex vert_data[3]
         {
-            fae::log(w, h);
+            {0.0f, 0.5f},
+            {0.5f, -0.5f},
+            {-0.5f, -0.5f}
         };
-        args.window_ptr->on_resize.add_callback(func);
+        mesh_->set_verts(vert_data, 3);
+        fae::triangle tri_data[1]
+        {
+            {0, 1, 2}
+        };
+        mesh_->set_tris(tri_data, 1);
     }
 
     virtual void unload(update_args & args) override
     {
-        fae::log("unload");
-        args.window_ptr->on_resize.remove_callback(func);
-        func = nullptr;
+        mesh_.reset();
+        shader_.reset();
     }
 
     virtual void update(update_args & args) override
     {
-        ++count;
-        if (count % 100 == 0)
-        {
-            fae::log("update");
-        }
     }
 
     virtual void render(render_args & args) const override
     {
-        if (count % 150 == 0)
-        {
-            fae::log("render");
-        }
+        shader_->bind();
+        args.renderer_ptr->draw_mesh(*mesh_.get());
     }
 
 private:
-    int count;
-    void(*func)(int, int);
+    std::unique_ptr<fae::shader> shader_;
+    std::unique_ptr<fae::mesh> mesh_;
 };
 
 }
