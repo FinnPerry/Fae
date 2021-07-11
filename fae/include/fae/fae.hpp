55a48da670372d81c99628a967fe5c79756d0f35
Finn Perry <finnperry3@gmail.com>
2021-07-11 15:10:58 +1000

    Add header to include other headers.

---
+++ fae/include/fae/fae.hpp
@@ -0,0 +1,13 @@
+#ifndef FAE_FAE_GUARD
+#define FAE_FAE_GUARD
+
+#include "application.hpp"
+#include "entity.hpp"
+#include "event.hpp"
+#include "logger.hpp"
+#include "mesh.hpp"
+#include "renderer.hpp"
+#include "shader.hpp"
+#include "window.hpp"
+
+#endif
