5b90e546ad687c38d2ceacc41d8d0e02ce8b1c42
FinnPerry <finnperry3@gmail.com>
2021-07-14 13:09:27 +1000

    Add unit test lib.

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,6 +1,38 @@
 #include <fae/fae.hpp>
 
+#include <cppunit/cppunit.h>
+
+namespace
+{
+    
+class unit_tests : public Cppunit
+{
+    virtual void test_list() override
+    {
+        test_0();
+        test_1();
+        test_2();
+    }
+    
+    void test_0()
+    {
+        CHECKT(true);
+    }
+
+    void test_1()
+    {
+        CHECK(1 + 1, 2);
+    }
+
+    void test_2()
+    {
+        CHECKS("a", "a");
+    }
+};
+    
+}
+
 int main()
 {
-    fae::log("hi i am a unit test");
+    return unit_tests{}.run();
 }
