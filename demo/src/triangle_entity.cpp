c6816deda738d402784ab3e208f4b4aef4ef7e1c
Finn Perry <finnperry3@gmail.com>
2021-07-17 19:16:51 +1000

    Update events to use std::function.

--- demo/src/triangle_entity.cpp
+++ demo/src/triangle_entity.cpp
@@ -2,11 +2,12 @@
 
 namespace
 {
-    
+
 void window_size_callback(int w, int h)
 {
     fae::log(w, h);
 }
+int window_size_callback_id;
     
 }
 
@@ -31,12 +32,12 @@ void triangle_entity::load(update_args const & args)
     };
     mesh_->set_tris(tri_data, 1);
     
-    args.window_ptr->on_resize.add_callback(window_size_callback);
+    window_size_callback_id = args.window_ptr->on_resize.add_callback(window_size_callback);
 }
 
 void triangle_entity::unload(update_args const & args)
 {
-    args.window_ptr->on_resize.remove_callback(window_size_callback);
+    args.window_ptr->on_resize.remove_callback(window_size_callback_id);
     mesh_.reset();
     shader_.reset();
 }
